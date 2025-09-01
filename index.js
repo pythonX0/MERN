55752b9d6cd6dcf7548514f89816a502ccc9561e
pythonX0 <pythonjrx@gmail.com>
2025-09-01 15:15:04 +0700

    Initialize project with index.js, jsconfig.json, and package.json; remove index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+const name = "zhangsan";
+const role = 'developer';
+if (role === 'developer') {
+    console.log('I am a developer');
+}
+// console.log("hello , world");
+// console.log(`hello, ${name} `);
+var oldway = "old way" 
+let modernway = "modern way"
+const constantway = "this value won't change";
+console.log(oldway);
+console.log(modernway);
+console.log(constantway);
